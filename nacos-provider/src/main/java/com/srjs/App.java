b448087fae7da074307912b1ce57f17d12a84851
刘锐 <liurui@daling.com>
2019-03-21 17:00:44 +0800

    初始化项目

---
+++ nacos-provider/src/main/java/com/srjs/App.java
@@ -0,0 +1,29 @@
+package com.srjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class App 
+{
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @RestController
+    public class EchoController {
+        @GetMapping(value = "/echo/{message}")
+        public String echo(@PathVariable String message) {
+            return "Hello Nacos Discovery " + message;
+        }
+    }
+}
